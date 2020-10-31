e181a1f88e90560a52c1773b5e71307a2f3bee96
Russell Nyorere <neorusse@gmail.com>
2020-10-31 11:14:11 +0100

    Replace default VPC

--- aws-ec2-terraform/terraform_config.tf
+++ aws-ec2-terraform/terraform_config.tf
@@ -10,6 +10,16 @@ variable "region" {
   default = "us-east-1"
 }
 
+# VPC Network Address Space
+variable "network_address_space" {
+  default = "10.1.0.0/16"
+}
+
+# Subnet 1 Network Address Space
+variable "subnet1_address_space" {
+  default = "10.1.0.0/24"
+}
+
 ##################################################################################
 # PROVIDERS
 ##################################################################################
@@ -24,6 +34,10 @@ provider "aws" {
 # DATA
 ##################################################################################
 
+# Retrieve the list of AZ in the region
+data "aws_availability_zones" "available" {}
+
+# Grab the most recent AMI of Amazon Linux
 data "aws_ami" "aws-linux" {
   most_recent = true
   owners      = ["amazon"]
@@ -49,43 +63,81 @@ data "aws_ami" "aws-linux" {
 # RESOURCES
 ##################################################################################
 
-#This uses the default VPC.  It WILL NOT delete it on destroy.
-resource "aws_default_vpc" "default" {
+# NETWORKING #
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.network_address_space
+  enable_dns_hostnames = "true"
 
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "linux_server"
-  description = "Allow ports for linux server"
-  vpc_id      = aws_default_vpc.default.id
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
 
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block              = var.subnet1_address_space
+  vpc_id                  = aws_vpc.vpc.id
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+
+}
+
+# ROUTING #
+resource "aws_route_table" "rtb" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# ROUTE TABLE SUBNET ASSOCIATION #
+resource "aws_route_table_association" "rta-subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rtb.id
+}
+
+# SECURITY GROUPS #
+resource "aws_security_group" "allow_ssh" {
+  name        = "sg_connect"
+  vpc_id      = aws_vpc.vpc.id
+
+  # SSH access from anywhere
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  # HTTP access from anywhere
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  # outbound internet access
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = -1
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_instance" "nginx" {
+# INSTANCES #
+resource "aws_instance" "linux_server" {
   ami                    = data.aws_ami.aws-linux.id
   instance_type          = "t2.micro"
-  key_name               = var.key_name
+  subnet_id              = aws_subnet.subnet1.id
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  key_name               = var.key_name
 
-    tags = {
+  tags = {
     Name = "linux_server"
   }
 
